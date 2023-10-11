3791c591dee0f7f26fc584017971fc6f55c2c717
roshameow <38164553+roshameow@users.noreply.github.com>
2023-10-11 03:11:21 +0000

    Deploying to gh-pages from  @ ed01f0d6cab18754a479a422381f9a8891514d79 🚀

---
+++ assets/activity_template-eb198dc4.js
@@ -0,0 +1,1 @@
+import{_ as n,o as a,c as o,a as t,r as s}from"./index-3f528a7c.js";const c={},_={class:"item"},d={class:"details"};function i(e,l){return a(),o("div",_,[t("i",null,[s(e.$slots,"icon",{},void 0,!0)]),t("div",d,[t("h3",null,[s(e.$slots,"heading",{},void 0,!0)]),s(e.$slots,"default",{},void 0,!0)])])}const u=n(c,[["render",i],["__scopeId","data-v-597c29a4"]]);export{u as A};
